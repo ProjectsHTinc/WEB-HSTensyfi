3c095f9ce09ba74114634155b707b36c18885228
kamalhappysanz <kamal.happysanz@gmail.com>
2017-08-08 18:19:23 +0530

    promotion
    committed

---
+++ application/models/Promotionmodel.php
@@ -0,0 +1,47 @@
+<?php
+
+Class Promotionmodel extends CI_Model
+{
+
+    public function __construct()
+     {
+        parent::__construct();
+     }
+
+
+
+     function getYear()
+     {
+       $sqlYear = "SELECT * FROM edu_academic_year WHERE NOW() >= from_month AND NOW() <= to_month AND status = 'Active'";
+       $year_result = $this->db->query($sqlYear);
+       $ress_year = $year_result->result();
+
+       if($year_result->num_rows()==1)
+       {
+         foreach ($year_result->result() as $rows)
+         {
+             $year_id = $rows->year_id;
+         }
+         return $year_id;
+       }
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+	?>
